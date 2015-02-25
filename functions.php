1ff10652ba60bb521165b20e84367f0b171dc934
fatesinger <jigoulee@gmail.com>
2015-02-25 15:40:34 +0800

    add http method

--- functions.php
+++ functions.php
@@ -1,1 +1,28 @@
 <?php
+
+function oauth_http($method,$header,$url,$data){
+	$method = $method ? $method : 'get';
+	if( $method == 'get') {
+		$ch = curl_init ();
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+		curl_setopt ( $ch, CURLOPT_URL, $url );
+		$result = curl_exec ( $ch );
+		curl_close ( $ch );
+		return $result;
+	
+	} elseif ( $method == 'post' ) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+    curl_setopt ( $ch, CURLOPT_POST, TRUE );
+    curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+    curl_setopt ( $ch, CURLOPT_URL, $url );
+    curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    $ret = curl_exec ( $ch );
+    curl_close ( $ch );
+    return $ret;
+	
+	}
+
+}	
