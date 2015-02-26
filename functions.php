f6f163b6d81053d9e4703dd2b8eddf485c68135a
fatesinger <jigoulee@gmail.com>
2015-02-26 21:01:55 +0800

    add wechat oauth2.0
    only a test

--- functions.php
+++ functions.php
@@ -1,28 +1,42 @@
 <?php
-
+require('wechat.php');
 function oauth_http($method,$header,$url,$data){
-	$method = $method ? $method : 'get';
-	if( $method == 'get') {
-		$ch = curl_init ();
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
-		curl_setopt ( $ch, CURLOPT_URL, $url );
-		$result = curl_exec ( $ch );
-		curl_close ( $ch );
-		return $result;
-	
-	} elseif ( $method == 'post' ) {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-    curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
-    curl_setopt ( $ch, CURLOPT_POST, TRUE );
-    curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
-    curl_setopt ( $ch, CURLOPT_URL, $url );
-    curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    $ret = curl_exec ( $ch );
-    curl_close ( $ch );
-    return $ret;
-	
-	}
+    $method = $method ? $method : 'get';
+    if( $method == 'get') {
+        $ch = curl_init ();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        $result = curl_exec ( $ch );
+        curl_close ( $ch );
+        return $result;
 
-}	
+    } elseif ( $method == 'post' ) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+        curl_setopt ( $ch, CURLOPT_POST, TRUE );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $ret = curl_exec ( $ch );
+        curl_close ( $ch );
+        return $ret;
+
+    }
+
+}
+
+
+
+function social_oauth(){
+    if (isset($_GET['code']) && isset($_GET['type']) && $_GET['type'] == 'weixin'){
+
+        wechat_oauth();
+
+    }
+
+}
+add_action('init','social_oauth')
+
+
---
+++ wechat.php
@@ -0,0 +1,60 @@
+<?php
+
+define('WX_APPID','');
+define('WX_APPSECRET','');
+function wechat_oauth(){
+    $code = $_GET['code'];
+    $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" . WX_APPID . "&secret=" . WX_APPSECRET . "&code=" . $code . "&grant_type=authorization_code";
+    $content = oauth_http('get',$array(),$url,'');
+    $ss = json_decode($content,true);
+    $info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token=' . $ss['access_token'] . '&openid=' . $ss['openid'];
+    $user_info = oauth_http('get',$array(),$info_url,'');
+    $weixin_id = $user_info["unionid"];
+    if(is_user_logged_in()){
+        $this_user = wp_get_current_user();
+        update_user_meta($this_user->ID ,"weixin_uid",$weixin_id);
+        //update_user_meta($this_user->ID ,"weixin_access_token",$weixin_access_token);
+        echo '<script>if( window.opener ) {window.opener.location.reload();
+						window.close();
+						}else{
+						window.location.reload()";
+						}</script>';
+    }else{
+        $oauth_user = get_users(array("meta_key "=>"weixin_uid","meta_value"=>$weixin_id));
+        if(is_wp_error($oauth_user) || !count($oauth_user)){
+            $username = $user_info["nickname"];
+            $login_name = wp_create_nonce($weixin_id);
+            $random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
+            $userdata=array(
+                'user_login' => $login_name,
+                'display_name' => $username,
+                'user_pass' => $random_password,
+                'nick_name' => $username
+            );
+            $user_id = wp_insert_user( $userdata );
+            wp_signon(array("user_login"=>$login_name,"user_password"=>$random_password),false);
+            update_user_meta($user_id ,"weixin_uid",$weixin_id);
+            echo '<script>if( window.opener ) {window.opener.location.reload();
+						window.close();
+						}else{
+						window.location.href = "'.home_url('/me/setting?2').'";
+						}</script>';
+
+        }else{
+            //update_user_meta($oauth_user[0]->ID ,"weixin_access_token",$sina_access_token);
+            wp_set_auth_cookie($oauth_user[0]->ID);
+            echo '<script>if( window.opener ) {window.opener.location.reload();
+						window.close();
+						}else{
+						window.location.href = "'.home_url('/me/setting?2').'";
+						}</script>';
+        }
+    }
+}
+
+function wechat_oauth_url(){
+    $_SESSION ['state'] = md5 ( uniqid ( rand (), true ) );
+    $appkey = '';
+    $url = 'https://open.weixin.qq.com/connect/qrconnect?appid='. WX_APPID .'&redirect_uri='. urlencode (home_url('/type=wechat')) .'&response_type=code&scope=snsapi_login&state=' . $_SESSION ['state'] . '#wechat_redirect'
+	return $url;
+}
