e196f0042a57b651994c4ae990ac99358c1df523
bigfa <tinyglim@gmail.com>
2015-09-26 02:41:01 +0800

    avatar hook

--- functions.php
+++ functions.php
@@ -26,4 +26,37 @@ function oauth_http($method,$header,$url,$data){
 
 }
 
+function fa_avatar_hook( $avatar, $id_or_email, $size, $default, $alt ) {
+    $user = false;
+
+    if ( is_numeric( $id_or_email ) ) {
+
+        $id = (int) $id_or_email;
+        $user = get_user_by( 'id' , $id );
+
+    } elseif ( is_object( $id_or_email ) ) {
+
+        if ( ! empty( $id_or_email->user_id ) ) {
+            $id = (int) $id_or_email->user_id;
+            $user = get_user_by( 'id' , $id );
+        }
+
+    } else {
+        $user = get_user_by( 'email', $id_or_email );   
+    }
+
+    if ( $user && is_object( $user ) ) {
+            if( get_user_meta($user->data->ID,'weixin_avatar',true) ){
+            $avatar = get_user_meta($user->data->ID,'weixin_avatar',true);
+            $avatar =  str_replace('http','https',$avatar);
+            $avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+        } else if( get_user_meta($user->data->ID,'sina_avatar',true) ){
+            $avatar = get_user_meta($user->data->ID,'sina_avatar',true);
+            $avatar = "<img alt='{$alt}' src='{$avatar}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+        }//根据你的存储头像的key来写
+    }
+
+    return $avatar;
+}
+add_filter('get_avatar', 'fa_avatar_hook' , 1 , 5);
 
