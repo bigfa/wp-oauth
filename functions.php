b494ebca73276bf222d678c4d997b235e07320ae
bigfa <jigoulee@gmail.com>
2015-11-07 00:40:41 +0800

    添加公共跳转函数

--- functions.php
+++ functions.php
@@ -1,31 +1,23 @@
 <?php
-function oauth_http($method,$header,$url,$data){
-    $method = $method ? $method : 'get';
-    if( $method == 'get') {
-        $ch = curl_init ();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
-        curl_setopt ( $ch, CURLOPT_URL, $url );
-        $result = curl_exec ( $ch );
-        curl_close ( $ch );
-        return $result;
+/*
 
-    } elseif ( $method == 'post' ) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, TRUE );
-        curl_setopt ( $ch, CURLOPT_POST, TRUE );
-        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
-        curl_setopt ( $ch, CURLOPT_URL, $url );
-        curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        $ret = curl_exec ( $ch );
-        curl_close ( $ch );
-        return $ret;
+公共跳转函数
 
-    }
-
+*/
+function fa_auth_redirect(){
+    echo '<script>if( window.opener ) {
+            window.opener.location.reload();
+            window.close();
+          } else {
+             window.location.href = "'.home_url().'";
+          }</script>';
 }
 
+/*
+
+Gravatar 头像 Hook， 包含微信、微博、QQ头像。
+
+*/
 function fa_avatar_hook( $avatar, $id_or_email, $size, $default, $alt ) {
     $user = false;
 
